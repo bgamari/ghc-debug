1937448d1b78f13934ce8d695f962e4ea7fff58d
Matthew Pickering <matthewtpickering@gmail.com>
2019-09-04 12:19:32 +0100

    Add dyepack test

---
+++ dyepack-test/Main.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE DeriveGeneric #-}
+
+module Main where
+
+import Debug.Dyepack
+import GHC.Generics
+import Control.Concurrent
+import GHC.Debug.Stub
+
+data A = A String deriving Show
+data User = User A Int deriving Generic
+
+main = do
+  start
+  v <- getLine
+  let a = A v
+      y = id [a, a, a]
+      u = User a 5
+
+  dyed <- dye u
+  -- This function will break the debugger if `a` is retained at this
+  -- point
+
+  checkDyed dyed (\s x -> do
+                    print ("LEAKED: PAUSING:" ++ s)
+                    saveClosures [Box x]
+                    pause
+                    threadDelay 10000000)
+  -- y retains a reference to a, so it can't be gced
+  print y
+  print ()
+
