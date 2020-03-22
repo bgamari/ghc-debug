06c7862b305ad2866b0945a36f8dd340d9d69791
Sven Tennie <sven.tennie@gmail.com>
2020-03-22 16:24:24 +0100

    system-tests: Add test for RequestRoots

--- test/test/SystemTest.hs
+++ test/test/SystemTest.hs
@@ -26,7 +26,7 @@ spec = do
               Nothing -> error "No Dwarf"
 
   describe "request" $ do
-    describe "version" $ do
+    describe "RequestVersion" $ do
       it "should return the correct version" $ do
         withTempDir $ \ tempDirPath -> do
           let socketName = tempDirPath ++ "/ghc-debug"
@@ -34,9 +34,18 @@ spec = do
             prog <- readCreateProcess (shell "which debug-test") []
             withDebuggee (trim prog) socketName $ \ d -> do
               version <- request d RequestVersion
-              request d RequestPause
-              request d RequestResume
               version `shouldBe` 0
+
+    describe "RequestRoots" $ do
+      it "should return a non-empty result" $ do
+        withTempDir $ \ tempDirPath -> do
+          let socketName = tempDirPath ++ "/ghc-debug"
+          withServer "debug-test" socketName True $ \serverIn serverOut serverProc -> do
+            prog <- readCreateProcess (shell "which debug-test") []
+            withDebuggee (trim prog) socketName $ \ d -> do
+              request d RequestPause
+              roots <- request d RequestRoots
+              roots `shouldNotBe` []
 
 shouldContainCuName :: Dwarf -> String -> Expectation
 shouldContainCuName dwarf name = allCuNames `shouldContain` [name]
