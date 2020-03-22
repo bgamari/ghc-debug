d1f2f980f2c402d9c558e58d79865b0003d80d2d
Sven Tennie <sven.tennie@gmail.com>
2020-03-22 16:15:02 +0100

    system-tests: Add test for RequestVersion

--- test/test/SystemTest.hs
+++ test/test/SystemTest.hs
@@ -25,6 +25,19 @@ spec = do
               Just dwarf -> dwarf `shouldContainCuName` "Test.hs"
               Nothing -> error "No Dwarf"
 
+  describe "request" $ do
+    describe "version" $ do
+      it "should return the correct version" $ do
+        withTempDir $ \ tempDirPath -> do
+          let socketName = tempDirPath ++ "/ghc-debug"
+          withServer "debug-test" socketName True $ \serverIn serverOut serverProc -> do
+            prog <- readCreateProcess (shell "which debug-test") []
+            withDebuggee (trim prog) socketName $ \ d -> do
+              version <- request d RequestVersion
+              request d RequestPause
+              request d RequestResume
+              version `shouldBe` 0
+
 shouldContainCuName :: Dwarf -> String -> Expectation
 shouldContainCuName dwarf name = allCuNames `shouldContain` [name]
   where
