ed5833bba3ace60aa7ef943cc4343ea284026ba2
Sven Tennie <sven.tennie@gmail.com>
2020-04-05 19:27:37 +0200

    system-tests: Add test for RequestSavedObjects

--- test/test/Server.hs
+++ test/test/Server.hs
@@ -1,4 +1,4 @@
-module Server (withServer, withStartedDebuggee) where
+module Server (withServer, withStartedDebuggee, withStartedDebuggeeAndHandles, Handles(..)) where
 
 import Control.Concurrent.Async
 import Control.Monad
@@ -19,6 +19,7 @@ withServer serverExe socketName logStdErr f = do
         std_err = CreatePipe,
         env = Just [("GHC_DEBUG_SOCKET",socketName)]
         }
+-- TODO pattern match case where one or more handles are not available (-> error with message)
   withCreateProcess p $ \(Just serverIn) (Just serverOut) (Just serverErr) serverProc -> do
     -- Need to continuously consume to stderr else it gets blocked
     -- Can't pass NoStream either to std_err
@@ -26,6 +27,7 @@ withServer serverExe socketName logStdErr f = do
     hSetBinaryMode serverErr True
     let errSinkThread = forever $ hGetLine serverErr >>= when logStdErr . putStrLn
     withAsync errSinkThread $ \_ -> f serverIn serverOut serverProc
+
 
 withStartedDebuggee :: String  -- ^ executable name
              -> (Debuggee -> IO a) -- ^ action
@@ -35,5 +37,23 @@ withStartedDebuggee exeName action = withTempDir $ \ tempDirPath -> do
   withServer exeName socketName True $ \serverIn serverOut serverProc -> do
     prog <- readCreateProcess serverExePathCmd []
     withDebuggee (trim prog) socketName action
+  where
+    serverExePathCmd = shell $ "which " ++ exeName
+
+data Handles = Handles {
+  stdin :: Handle,
+  stdout :: Handle,
+  process :: ProcessHandle
+                       }
+
+withStartedDebuggeeAndHandles :: String  -- ^ executable name
+             -> (Handles -> Debuggee -> IO a) -- ^ action
+             -> IO a
+withStartedDebuggeeAndHandles exeName action = withTempDir $ \ tempDirPath -> do
+  let socketName = tempDirPath ++ "/ghc-debug"
+  withServer exeName socketName True $ \serverIn serverOut serverProc -> do
+    prog <- readCreateProcess serverExePathCmd []
+    let handles = Handles serverIn serverOut serverProc
+    withDebuggee (trim prog) socketName (action handles)
   where
     serverExePathCmd = shell $ "which " ++ exeName
--- test/test/SystemTest.hs
+++ test/test/SystemTest.hs
@@ -4,10 +4,16 @@ import Test.Hspec
 
 import GHC.Debug.Client
 import Data.Text (unpack)
-
+import System.IO
 import Data.Dwarf.ADT
 
 import Server
+
+import Debug.Trace
+import Control.Monad
+
+import Control.Concurrent.Async
+import Control.Concurrent
 
 spec :: SpecWith ()
 spec = do
@@ -28,9 +34,41 @@ spec = do
     describe "RequestRoots" $
       it "should return a non-empty result" $
         withStartedDebuggee "debug-test" $ \ d -> do
-              request d RequestPause
-              roots <- request d RequestRoots
-              roots `shouldNotBe` []
+          request d RequestPause
+          roots <- request d RequestRoots
+          roots `shouldNotBe` []
+
+    describe "RequestSavedObjects" $
+      it "should return saved object" $
+        withStartedDebuggeeAndHandles "save-one" $ \ h d -> do
+          waitForSync $ Server.stdout h
+          let errSinkThread = forever $ do
+                l <- hGetLine (Server.stdout h)
+                print l
+          withAsync errSinkThread $ \_ -> do
+            -- TODO Get rid of the `threadDelay`. `save-one` should signal that the GC has finished.
+            threadDelay 5000000
+            request d RequestPause
+            ss@(s:_) <- request d RequestSavedObjects
+            length ss `shouldBe` 1
+            sos <- request d (RequestFindPtr s)
+            print $ "sos : " ++ show sos
+            length sos `shouldBe` 1
+            dcs <- dereferenceClosures d sos
+            mapM_ print dcs
+            length dcs `shouldBe` 1
+
+waitForSync :: Handle -> IO ()
+waitForSync h = do
+  hSetBuffering h LineBuffering
+  l <- hGetLine h
+  traceIO $ "line " ++ l
+  if l == "\"sync\"" then
+    return ()
+  else
+    waitForSync h
+-- TODO There should be some exit condition.
+--    error "Can not sync!"
 
 shouldContainCuName :: Dwarf -> String -> Expectation
 shouldContainCuName dwarf name = allCuNames `shouldContain` [name]
