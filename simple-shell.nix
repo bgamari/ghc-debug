13626a02c8256556f54f90523977f284d5a67363
Sven Tennie <sven.tennie@gmail.com>
2020-04-22 09:44:49 +0200

    Update used GHC

--- simple-shell.nix
+++ simple-shell.nix
@@ -15,12 +15,18 @@ let
     sha256 = "1qz1lwk0j2r6jzpc5ssrlfcf1193ay5l23kwyda5wld5sm170ffw";
   }) { pkgs = np; }).mkGhc;
 
+  # This archive will be gone on 20th May. Please update when a new build of the
+  # `wip/ghc-debug` branch becomes available (which will hold for another month).
   ghc = mkGhc
-        { url = "https://gitlab.haskell.org/ghc/ghc/-/jobs/150213/artifacts/raw/ghc-x86_64-fedora27-linux.tar.xz";
-          hash = "02ympcnm33msdl4kv10nb1s44vc9mrsrgwgylis3l8ncr1564igy"; };
+        { url = "https://gitlab.haskell.org/ghc/ghc/-/jobs/312946/artifacts/raw/ghc-x86_64-fedora27-linux.tar.xz";
+          hash = "1gw3q6g059yz0lqd9x3r23isghdwqw5zimysm73pp26jdg3rsc2w";
+        };
 
 in
   _np.mkShell { buildInputs = [ ghc
                                np.ncurses
                                np.cabal-install
-                             ]; }
+                               np.zlib
+                               np.elfutils
+                              ];
+              }
