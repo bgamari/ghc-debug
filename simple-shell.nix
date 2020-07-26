954db9e2791b8dc922a441f22839cfff302f23a9
Sven Tennie <sven.tennie@gmail.com>
2020-07-26 17:15:49 +0200

    Update simple-shell.nix
    Use a current artifact of wip/ghc-debug.

--- simple-shell.nix
+++ simple-shell.nix
@@ -4,34 +4,48 @@ let
   np = import (_np.fetchFromGitHub {
     owner  = "NixOS";
     repo   = "nixpkgs";
-    rev    = "6d445f8398d2d585d20d9acacf00fd9d15081b3b";
-    sha256 = "1ajd0zr31iny3g9hp0pc1y2pxcm3nakdv6260lnvyn0k4vygync2";
+    rev    = "5272327b81ed355bbed5659b8d303cf2979b6953";
+    sha256 = "0182ys095dfx02vl2a20j1hz92dx3mfgz2a6fhn31bqlp1wa8hlq";
   }) {};
 
   mkGhc = (import (np.fetchFromGitHub {
-    owner  = "mpickering";
-    repo   = "old-ghc-nix";
-    rev    = "be3ca79e4e17cf7046110f14379307f3d348a702";
-    sha256 = "1qz1lwk0j2r6jzpc5ssrlfcf1193ay5l23kwyda5wld5sm170ffw";
+    owner = "supersven";
+    repo = "old-ghc-nix";
+    rev = "093fe8273876ab1b746f6f3301a27aa2dd93ed75";
+    sha256 = "186j36idapllxllwcw8svkzf8zvadvls2y4kw0mx4syc6ap4glrx";
   }) { pkgs = np; }).mkGhc;
 
-  # This archive will be gone on 20th May. Please update when a new build of the
-  # `wip/ghc-debug` branch becomes available (which will hold for another month).
+  # Cached artifacts do not last forever!
+  # If this fails, please update url and hash for the lastest successful build
+  # of wip/ghc-debug.
   ghc = mkGhc
-        { url = "https://gitlab.haskell.org/ghc/ghc/-/jobs/312946/artifacts/raw/ghc-x86_64-fedora27-linux.tar.xz";
-          hash = "1gw3q6g059yz0lqd9x3r23isghdwqw5zimysm73pp26jdg3rsc2w";
+        { url = "https://gitlab.haskell.org/ghc/ghc/-/jobs/401867/artifacts/raw/ghc-x86_64-fedora27-linux.tar.xz";
+          hash = "0682i88znps8kk1ljjpzhg00z7lrp7g920ra9zc3hlbrg8x0n6rh";
         };
 
 in
   _np.mkShell { buildInputs = [ ghc
                                np.ncurses
                                np.cabal-install
-                               np.zlib
+                               np.zlib.dev
+                               np.zlib.out
                                np.elfutils
                                np.git
+                               np.numactl
+                               np.pkg-config
+
+                               np.gmp
+                               np.gmp.dev
                               ];
 
                 # Export the location of the SSL CA bundle
                 SSL_CERT_FILE = "${np.cacert}/etc/ssl/certs/ca-bundle.crt";
                 NIX_SSL_CERT_FILE = "${np.cacert}/etc/ssl/certs/ca-bundle.crt";
+                shellHook = ''
+                  export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${_np.lib.makeLibraryPath [np.numactl np.gmp]}";
+                  unset LD
+
+                  echo 'Please build with --extra-include-dirs=${np.zlib.dev}/include/, e.g.:'
+                  echo cabal 'new-build all --extra-include-dirs=${np.zlib.dev}/include/'
+                '';
               }
