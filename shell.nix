0c1119735e7bd5137408e41625207b16fecef92b
Matthew Pickering <matthewtpickering@gmail.com>
2020-12-04 09:57:00 +0000

    Add patched cabal to shell.nix

--- shell.nix
+++ shell.nix
@@ -2,9 +2,14 @@ let
   np = import ((import <nixpkgs> { }).fetchFromGitHub {
     owner  = "NixOS";
     repo   = "nixpkgs";
-    rev    = "5272327b81ed355bbed5659b8d303cf2979b6953";
-    sha256 = "0182ys095dfx02vl2a20j1hz92dx3mfgz2a6fhn31bqlp1wa8hlq";
-  }) {};
+    rev    = "0da76dab4c2acce5ebf404c400d38ad95c52b152";
+    sha256 = "1lj3h4hg3cnxl3avbg9089wd8c82i6sxhdyxfy99l950i78j0gfg";
+  }) { overlays = [(sel: sup: { allCabalHashes = newHashes sup; })]; };
+
+  newHashes = super: super.fetchurl {
+    url    = "https://github.com/commercialhaskell/all-cabal-hashes/archive/0b609355609de036fd5d0764e946e1ee33acda59.tar.gz";
+    sha256 = "1qbzdngm4q8cmwydnrg7jvipw39nb1mjxw95vw6f789874002kn1";
+  };
 
   mkGhc = (import (np.fetchFromGitHub {
     owner = "supersven";
@@ -12,6 +17,24 @@ let
     rev = "093fe8273876ab1b746f6f3301a27aa2dd93ed75";
     sha256 = "186j36idapllxllwcw8svkzf8zvadvls2y4kw0mx4syc6ap4glrx";
   }) { pkgs = np; }).mkGhc;
+
+  cabalFork = (np.fetchFromGitHub {
+    owner = "bgamari";
+    repo = "cabal";
+    rev = "8a9621cdc6ffbe5c2c4512559e00c1b419e1e9cf";
+    sha256 = "14jgpcvafibj285ywkkayy336xfpdj5akmj09rsrpmqdqymcwqwx";
+  });
+
+  hps = (np.haskellPackages.extend (self: super: rec {
+    Cabal = np.haskell.lib.dontCheck (super.callCabal2nix "Cabal" "${cabalFork}/Cabal" {});
+    random = np.haskell.lib.dontCheck (super.callHackage "random" "1.2.0" { });
+    splitmix = np.haskell.lib.dontCheck super.splitmix_0_1_0_3;
+    mkDerivation = args: super.mkDerivation (args // {
+      doCheck = false;
+    });
+  }));
+
+  fixedCabal = np.haskell.lib.dontCheck (hps.callCabal2nix "cabal-install" "${cabalFork}/cabal-install" { });
 
   # Cached artifacts do not last forever!
   # If this fails, please update url and hash for the lastest successful build
@@ -23,8 +46,9 @@ let
   ghc-utils = import ../ghc-utils {};
 
 in
-  np.mkShell { buildInputs = [ ghc
+  np.mkShell { buildInputs = [
                                ghc-utils
+                               fixedCabal
                                np.linuxPackages.perf
                                np.ncurses
                                np.wget  # Used by cabal-install for https support when communicating with head.hackage
@@ -45,9 +69,5 @@ in
                 NIX_SSL_CERT_FILE = "${np.cacert}/etc/ssl/certs/ca-bundle.crt";
                 shellHook = ''
                   export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${np.lib.makeLibraryPath [np.numactl np.gmp]}";
-                  unset LD
-
-                  echo 'Please build with --extra-include-dirs=${np.zlib.dev}/include/, e.g.:'
-                  echo cabal 'new-build all --extra-include-dirs=${np.zlib.dev}/include/'
                 '';
               }
