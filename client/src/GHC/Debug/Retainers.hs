26673a2920b43473445fa7a812af20e9e71f4aff
Matthew Pickering <matthewtpickering@gmail.com>
2021-01-14 14:47:26 +0000

    Generalise findRetainers

--- client/src/GHC/Debug/Retainers.hs
+++ client/src/GHC/Debug/Retainers.hs
@@ -1,5 +1,5 @@
 -- | Functions for computing retainers
-module GHC.Debug.Retainers(findRetainers) where
+module GHC.Debug.Retainers(findRetainersOf, findRetainers) where
 
 import GHC.Debug.Client
 import Control.Monad.State
@@ -12,14 +12,21 @@ addOne :: [ClosurePtr] -> (Maybe Int, [[ClosurePtr]]) -> (Maybe Int, [[ClosurePt
 addOne _ (Just 0, cp) = (Just 0, cp)
 addOne cp (n, cps)    = (subtract 1 <$> n, cp:cps)
 
+findRetainersOf :: Maybe Int
+                -> [ClosurePtr]
+                -> [ClosurePtr]
+                -> DebugM [[ClosurePtr]]
+findRetainersOf limit cps bads = findRetainers limit cps (\cp _ -> return (cp `Set.member` bad_set))
+  where
+    bad_set = Set.fromList bads
+
 -- | From the given roots, find any path to one of the given pointers.
 -- Note: This function can be quite slow! The first argument is a limit to
 -- how many paths to find. You should normally set this to a small number
 -- such as 10.
-findRetainers :: Maybe Int -> [ClosurePtr] -> [ClosurePtr] -> DebugM [[ClosurePtr]]
-findRetainers limit rroots bads = (\(_, r, _) -> snd r) <$> runRWST (traceFromM funcs rroots) [] (limit, [])
+findRetainers :: Maybe Int -> [ClosurePtr] -> (ClosurePtr -> SizedClosure -> DebugM Bool) -> DebugM [[ClosurePtr]]
+findRetainers limit rroots p = (\(_, r, _) -> snd r) <$> runRWST (traceFromM funcs rroots) [] (limit, [])
   where
-    bads_set = Set.fromList bads
     funcs = TraceFunctions {
                papTrace = const (return ())
               , stackTrace = const (return ())
@@ -33,13 +40,15 @@ findRetainers limit rroots bads = (\(_, r, _) -> snd r) <$> runRWST (traceFromM 
                -> SizedClosure
                -> RWST [ClosurePtr] () (Maybe Int, [[ClosurePtr]]) DebugM ()
                -> RWST [ClosurePtr] () (Maybe Int, [[ClosurePtr]]) DebugM ()
-    closAccum cp _ k
-      | cp `Set.member` bads_set = do
+    closAccum cp sc k = do
+      b <- lift $ p cp sc
+      if b
+        then do
           ctx <- ask
           modify' (addOne (cp: ctx))
           -- Don't call k, there might be more paths to the pointer but we
           -- probably just care about this first one.
-      | otherwise = do
+        else do
           (lim, _) <- get
           case lim of
             Just 0 -> return ()
