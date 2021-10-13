64634aaef67fb2f267f88e192c361f68409b7878
Matthew Pickering <matthewtpickering@gmail.com>
2021-10-13 09:47:06 +0100

    Add snapshotDirectory convention

--- convention/src/GHC/Debug/Convention.hs
+++ convention/src/GHC/Debug/Convention.hs
@@ -1,11 +1,18 @@
-module GHC.Debug.Convention (socketDirectory) where
+
+module GHC.Debug.Convention (socketDirectory, snapshotDirectory, ghcDebugDirectory) where
 
 import System.FilePath
 import System.Directory
 
 -- | The default socket directory in which to place unix domain sockets in
--- ghc-debug-stub.
-socketDirectory :: IO FilePath
-socketDirectory = do
+-- ghc-debug-stub. This is currently your XDG data directory.
+ghcDebugDirectory :: IO FilePath
+ghcDebugDirectory = do
     xdgDir <- getXdgDirectory XdgData ""
     return (xdgDir </> "ghc-debug/debuggee/")
+
+socketDirectory :: IO FilePath
+socketDirectory = (</> "sockets")  <$> ghcDebugDirectory
+
+snapshotDirectory :: IO FilePath
+snapshotDirectory = (</> "snapshots")  <$> ghcDebugDirectory
